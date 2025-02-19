41b8024fdb17afd26de75852e3582084cd4d6ea2
kimsengku <rlarjddnr135@gmail.com>
2025-02-19 16:23:17 +0900

    feat.KimSengUk [update]

--- controller/state.js
+++ controller/state.js
@@ -1,6 +1,6 @@
 const pool = require("../DB/db");
 
 exports.getState = async (req, res) => {
-  const user = req.session.user;
-  res.send(user);
+  const user = req.session.user || null;
+  res.json({ user });
 };
